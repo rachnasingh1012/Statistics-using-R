9dcb528d92e3cfbdd86f3f53dfe5fb4bd3ab19bd
rachnasingh1012 <75220099+rachnasingh1012@users.noreply.github.com>
2021-05-03 15:57:02 +0200

    Add files via upload

---
+++ Basic Operations.R
@@ -0,0 +1,23 @@
+# Basic Mathematic Operation
+
+# Addition
+
+4+9
+
+# Sustraction 
+
+9-7
+
+# Multiply
+
+9*5
+
+# Division
+
+8/4
+
+#Using parentheses
+
+4 + 3 * 8
+(4 + 3) * 8
+(3.8^7.2) * (3 - 2)
---
+++ Variable assignment & checking of Data Type.R
@@ -0,0 +1,19 @@
+#Variable assignment
+
+my_apples <- 5
+my_apples
+my_apples <- 4
+my_apples
+my_oranges <- 7
+my_fruit <- my_apples + my_oranges
+my_fruit
+
+#Checking the data type
+
+my_numeric <- 42
+my_character <- "birds"
+my_logical <- TRUE
+
+class(my_numeric)
+class(my_character)
+class(my_logical)
